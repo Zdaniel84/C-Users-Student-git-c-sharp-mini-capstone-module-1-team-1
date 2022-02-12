7fbc82a0e00b792a27c0d036ee1ed5e0738d38e8
Mario Rizzardi <mrizzardi22@gmail.com>
2022-02-11 21:40:16 -0500

    Submitting project menu setup

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -28,7 +28,7 @@ namespace Capstone
 
                 if (mainMenuChoice == "1")
                 {
-                    
+
                     Console.Clear();
                     itemList.GetItems();
                     Console.WriteLine("Enter 'm' to return to main menu");
@@ -38,164 +38,151 @@ namespace Capstone
                     {
                         menuReturn.PrintMenu();
                         mainMenuChoice = Console.ReadLine();
+
                     }
 
                 }
-                Console.Clear();
+
 
 
 
                 if (mainMenuChoice == "2")
                 {
-
+                    Console.Clear();
                     Console.WriteLine("(1) Feed Money");
                     Console.WriteLine("(2) Select Product");
                     Console.WriteLine("(3) Finish Transaction");
 
                     Console.WriteLine("");
-                    Console.WriteLine("Current money provided: " + currentMoney);
+                    Console.WriteLine($"Current money provided: {currentMoney.ToString("C")}");
                     Console.WriteLine("");
                     Console.WriteLine("Select 1, 2, or 3");
                     string subMenuChoice = Console.ReadLine();
-                    while (goBack == true)
+
+                    if (subMenuChoice == "1")
                     {
-                        if (subMenuChoice == "1")
-                        {
-                            Console.WriteLine("Enter whole dollar amount: "); //Fix to follow readme
-                            string amountEntered = Console.ReadLine();
-                            Console.Clear();
-                            decimal balance = new decimal();
-                            // decimal balance = 0M;
-                            decimal.TryParse(amountEntered, out balance);
+                        Console.WriteLine("Enter whole dollar amount: "); //Fix to follow readme
+                        string amountEntered = Console.ReadLine();
+                        Console.Clear();
+                        decimal balance = new decimal();
+                        // decimal balance = 0M;
+                        decimal.TryParse(amountEntered, out balance);
 
 
-                            //sampleList.AddRange(items);
+                        //sampleList.AddRange(items);
 
-                            currentMoney += balance;
-                            Console.WriteLine("Balance: " + currentMoney);
-                        }
-                        itemList.GetItems();
-
-                        if (subMenuChoice == "2")
-                        {
-                            //Console.WriteLine("Select product (A1, B3, etc...)");
-                            //string userSelection = Console.ReadLine();
-
-                            ////Lots to add here
-
-                            //if (userSelection == "A1" || userSelection == "A2" || userSelection == "A3" || userSelection == "A4")
-                            //{
-                            //    Chips chips = new Chips();
-                            //    Console.WriteLine(chips.Sound);
-                            //}
-                            
-                            Console.WriteLine("Select Product code");
-                            Console.ReadLine();
-                            string userInput = "A1";
-
-
-                            for (int i = 0; i < sampleList.Count; i++)
-                            {
-                                if (sampleList[i].Equals(userInput))
-                                {
-                                    Console.WriteLine("your choice is " + sampleList[i + 1]);
-                                    Console.WriteLine("price: " + sampleList[i + 2]);
-                                    //string[] itemsAndStock = new string[5] {[i], [i + 1], [i + 2], [i + 3], [i + 4], "5"};
-                                }
-                            }
-
-                        }
-
+                        currentMoney += balance;
+                        Console.WriteLine("Balance: " + currentMoney);
                     }
+
+
+                    if (subMenuChoice == "2")
+                    {
+
+
+                        VendingItems choice = new VendingItems();
+
+                        choice.ProductSelection();
+
+                        //if (userSelection == "A1" || userSelection == "A2" || userSelection == "A3" || userSelection == "A4")
+                        //{
+                        //    Chips chips = new Chips();
+                        //    Console.WriteLine(chips.Sound);
+                        //}
+                    }
+                    if (subMenuChoice == "3")
+                    {
+                        Environment.Exit(0);
+                    }
+
+
                 }
-                else if (mainMenuChoice == "3")
+                else
                 {
 
                     Environment.Exit(0);
-                    goBack = false;
+
                 }
 
             }
 
 
-            while (mainMenuChoice == "2")
-            {
-                Console.WriteLine("(1) Feed Money");
-                Console.WriteLine("(2) Select Product");
-                Console.WriteLine("(3) Finish Transaction");
-                Console.WriteLine("Select item");
-                string subMenuChoice = Console.ReadLine();
+            //while (mainMenuChoice == "2")
+            //{
+            //    Console.WriteLine("(1) Feed Money");
+            //    Console.WriteLine("(2) Select Product");
+            //    Console.WriteLine("(3) Finish Transaction");
+            //    Console.WriteLine("Select item");
+            //    string subMenuChoice = Console.ReadLine();
 
-                if (subMenuChoice == "1")
-                {
-                    Console.WriteLine("Enter whole dollar amount"); //Fix to follow readme
-                    string amountEntered = Console.ReadLine();
-                    decimal numericalAmountEntered = new decimal();
-                    decimal balance = 0M;
-                    decimal.TryParse(amountEntered, out balance);
+            //    if (subMenuChoice == "1")
+            //    {
+            //        Console.WriteLine("Enter whole dollar amount"); //Fix to follow readme
+            //        string amountEntered = Console.ReadLine();
+            //        decimal numericalAmountEntered = new decimal();
+            //        decimal balance = 0M;
+            //        decimal.TryParse(amountEntered, out balance);
 
-                    //sampleList.AddRange(items);
+            //        //sampleList.AddRange(items);
 
-                    balance += numericalAmountEntered;
-                    Console.WriteLine("Balance: " + balance);
-                }
+            //        balance += numericalAmountEntered;
+            //        Console.WriteLine("Balance: " + balance);
+            //    }
 
-                if (subMenuChoice == "2")
-                {
-                    try
-                    {
-                        using (StreamReader stream = new StreamReader(path))
-                        {
+            //    if (subMenuChoice == "2")
+            //    {
+            //        try
+            //        {
+            //            using (StreamReader stream = new StreamReader(path))
+            //            {
 
-                            while (!stream.EndOfStream)
-                            {
+            //                while (!stream.EndOfStream)
+            //                {
 
-                                string line = stream.ReadLine();
-                                string[] items = line.Split("|");
-                                sampleList.AddRange(items);
+            //                    string line = stream.ReadLine();
+            //                    string[] items = line.Split("|");
+            //                    sampleList.AddRange(items);
 
-                                int stock = 5;
+            //                    int stock = 5;
 
-                                // Console.WriteLine(string.Join(", ", items) + ", " + stock);
+            //                    // Console.WriteLine(string.Join(", ", items) + ", " + stock);
 
-                            }
-                        }
-                    }
+            //                }
+            //            }
+            //        }
 
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Error reading file.");
-                    }
+            //        catch (Exception)
+            //        {
+            //            Console.WriteLine("Error reading file.");
+            //        }
 
-                    Console.WriteLine("Select product (A1, B3, etc...)");
-                    string userSelection = Console.ReadLine();
+            //        Console.WriteLine("Select product (A1, B3, etc...)");
+            //        string userSelection = Console.ReadLine();
 
-                    //Lots to add here
+            //        //Lots to add here
 
-                    if (userSelection == "A1" || userSelection == "A2" || userSelection == "A3" || userSelection == "A4")
-                    {
-                        Chips chips = new Chips();
-                        Console.WriteLine(chips.Sound);
-                    }
+            //        if (userSelection == "A1" || userSelection == "A2" || userSelection == "A3" || userSelection == "A4")
+            //        {
+            //            Chips chips = new Chips();
+            //            Console.WriteLine(chips.Sound);
+            //        }
 
-                    string userInput = userSelection;
+            //        string userInput = userSelection;
 
-                    for (int i = 0; i < sampleList.Count; i++)
-                    {
-                        if (sampleList[i].Equals(userInput))
-                        {
-                            Console.WriteLine("your choice is " + sampleList[i + 1]);
-                            Console.WriteLine("price: " + sampleList[i + 2]);
-                            //string[] itemsAndStock = new string[5] {[i], [i + 1], [i + 2], [i + 3], [i + 4], "5"};
-                        }
-                    }
+            //        for (int i = 0; i < sampleList.Count; i++)
+            //        {
+            //            if (sampleList[i].Equals(userInput))
+            //            {
+            //                Console.WriteLine("your choice is " + sampleList[i + 1]);
+            //                Console.WriteLine("price: " + sampleList[i + 2]);
+            //                //string[] itemsAndStock = new string[5] {[i], [i + 1], [i + 2], [i + 3], [i + 4], "5"};
+            //            }
+            //        }
 
-                    // subtract from balance
-                }
+            //        // subtract from balance
+            //    }
 
-            }
-
-
+            //}
 
 
 
